80f32d394ce4b3ca9508819531212d234c3dd7e1
Piero Mamberti <piero.mamberti@gmail.com>
2020-12-07 20:02:46 +0000

    Add Day2 second problem

--- day02.py
+++ day02.py
@@ -16,12 +16,22 @@ def check_password(password, char, min=0, max=0):
         return False
 
 
+def check_password_new_policy(password, char, pos_1, pos_2):
+    if (char == password[pos_1 - 1] and char != password[pos_2 - 1]) or (
+        char != password[pos_1 - 1] and char == password[pos_2 - 1]
+    ):
+        return True
+    else:
+        return False
+
+
 if __name__ == "__main__":
 
     passwords_list = []
 
     with open("day02_data", "r") as fin:
-        valid_count = 0
+        valid_count_old = 0
+        valid_count_new = 0
 
         for line in fin:
             passwords_list.append(line)
@@ -30,12 +40,26 @@ if __name__ == "__main__":
             requirements_min = int(raw_data[0].split("-")[0])
             requirements_max = int(raw_data[0].split("-")[1])
             pw_string = raw_data[2]
-            if check_password(
+
+            old_policy_check = check_password(
                 password=pw_string,
                 char=requirements_char,
                 max=requirements_max,
                 min=requirements_min,
-            ):
-                valid_count += 1
+            )
 
-        print(valid_count)
+            if old_policy_check:
+                valid_count_old += 1
+
+            new_policy_check = check_password_new_policy(
+                password=pw_string,
+                char=requirements_char,
+                pos_1=requirements_min,
+                pos_2=requirements_max,
+            )
+
+            if new_policy_check:
+                valid_count_new += 1
+
+        print(f"Old Policy Count of valid passwords: {valid_count_old}")
+        print(f"New Policy Count of valid passwords: {valid_count_new}")
