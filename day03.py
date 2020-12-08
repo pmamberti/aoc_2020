146a0c7d646ce0b6315da65effd2228b369b586f
Piero Mamberti <piero.mamberti@gmail.com>
2020-12-08 17:02:55 +0000

    Add day3 second problem

--- day03.py
+++ day03.py
@@ -1,27 +1,49 @@
-with open("day03_data", "r") as fin:
-    woods_map = fin.readlines()
+def create_map(x_increment):
+    with open("day03_data", "r") as fin:
+        woods_map = fin.readlines()
 
-# Create the map we need to not parse the \n cause I have no idea how to do that
-with open("day03_full_map", "w") as fout:
-    needed_steps = len(woods_map)
-    needed_columns = round(needed_steps / len(woods_map[0])) + 1
-    new_lines = [(line.strip() * needed_columns * 3) + "\n" for line in woods_map]
-    fout.writelines(new_lines)
+    # Create the map we need to not parse the \n cause I have no idea how to do that
+    with open("day03_full_map", "w") as fout:
+        needed_steps = len(woods_map)
+        needed_columns = round(needed_steps / len(woods_map[0])) + 1
+        new_lines = [
+            (line.strip() * needed_columns * x_increment) + "\n" for line in woods_map
+        ]
+        fout.writelines(new_lines)
 
-x = 0
-y = 0
-tree_count = 0
 
-with open("day03_full_map", "r") as fin:
-    full_map = fin.readlines()
-    print(len(full_map))
-    while y < len(full_map):
-        position = full_map[y][x]
-        print(y, len(position), position)
-        if position == "#":
-            tree_count += 1
+def count_trees(increment_x, increment_y):
+    x = 0
+    y = 0
+    tree_count = 0
 
-        x += 3
-        y += 1
+    with open("day03_full_map", "r") as fin:
+        full_map = fin.readlines()
+        while y < len(full_map):
+            position = full_map[y][x]
+            if position == "#":
+                tree_count += 1
 
-print(tree_count)
+            x += increment_x
+            y += increment_y
+
+    return tree_count
+
+
+if __name__ == "__main__":
+    slopes = [
+        {"increment_x": 1, "increment_y": 1},
+        {"increment_x": 3, "increment_y": 1},
+        {"increment_x": 5, "increment_y": 1},
+        {"increment_x": 7, "increment_y": 1},
+        {"increment_x": 1, "increment_y": 2},
+    ]
+
+    total_trees = 1
+
+    for slope in slopes:
+        create_map(x_increment=slope.get("increment_x"))
+        slope_count = count_trees(slope.get("increment_x"), slope.get("increment_y"))
+        total_trees *= slope_count
+
+    print(total_trees)
